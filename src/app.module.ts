ec4b4b861cabfb9513a45aed005e25acc5d76f77
Mrmark <93504873+suttirakcha@users.noreply.github.com>
2026-03-23 17:55:27 +0700

    feat(checklists): generate checklists resource (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { UsersModule } from './users/users.module';
+import { ChecklistsModule } from './checklists/checklists.module';
 
 @Module({
-  imports: [UsersModule, WorkspacesModule],
+  imports: [UsersModule, ChecklistsModule, WorkspacesModule],
   controllers: [AppController],
   providers: [AppService],
 })
