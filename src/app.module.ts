5afd66cfade30a9e360751bd22c5f8c3e524db7a
suttirakcha <suttirak.char@gmail.com>
2026-03-23 17:51:38 +0700

    feat(checklists): generate checklists resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ChecklistsModule } from './checklists/checklists.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ChecklistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
